8cf17d2bc1fa931d2765ec245c38724c559e6159
luiokx <lcsalvatierra.25@gmail.com>
2022-05-25 19:29:48 +0200

    change libvlc library

--- app/src/main/java/com/salvatierra/vlcmedia/MainActivity.java
+++ app/src/main/java/com/salvatierra/vlcmedia/MainActivity.java
@@ -28,7 +28,9 @@ public class MainActivity extends AppCompatActivity implements VideControllerVLC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //HIDE UI OF DEVICE
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
 
         initPlayer();
 
